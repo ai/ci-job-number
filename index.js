67e9118563385ad1c763d16819abab167aa8adb3
Andrey Sitnik <andrey@sitnik.ru>
2017-07-04 16:31:05 -0300

    Add JSDoc

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 'use strict'
 
+/**
+ * Returns job number if project is run on CI or `1` on local.
+ *
+ * @return {number} CI job number
+ *
+ * @example
+ * const ciJobNumber = require('ci-job-number')
+ *
+ * if (ciJobNumber() === 1) {
+ *   runSpellingCheck()
+ * } else {
+ *   console.warn('To speed up CI spelling check runs only in first job')
+ * }
+ */
 module.exports = function ciJobNumber () {
   if (process.env.TRAVIS) {
     return parseInt(process.env.TRAVIS_JOB_NUMBER.split('.')[1])
