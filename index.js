2c0133a91475bf270b8ddab541cfd9736556e9cd
Andrey Sitnik <andrey@sitnik.ru>
2017-07-04 11:03:05 -0300

    Add Travis and AppVeyor support

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 'use strict'
 
 module.exports = function ciJobNumber () {
-  return 1
+  if (process.env.TRAVIS) {
+    return parseInt(process.env.TRAVIS_JOB_NUMBER.split('.')[1])
+  } else if (process.env.APPVEYOR) {
+    return parseInt(process.env.APPVEYOR_JOB_NUMBER)
+  } else {
+    return 1
+  }
 }
--- index.test.js
+++ index.test.js
@@ -2,6 +2,23 @@
 
 const ciJobNumber = require('./')
 
+beforeEach(() => {
+  delete process.env.TRAVIS
+  delete process.env.APPVEYOR
+})
+
 it('returns 1 when run without CI', () => {
   expect(ciJobNumber()).toEqual(1)
+})
+
+it('supports Travis CI', () => {
+  process.env.TRAVIS = '1'
+  process.env.TRAVIS_JOB_NUMBER = '207.2'
+  expect(ciJobNumber()).toEqual(2)
+})
+
+it('supports AppVeyor', () => {
+  process.env.APPVEYOR = '1'
+  process.env.APPVEYOR_JOB_NUMBER = '3'
+  expect(ciJobNumber()).toEqual(3)
 })
